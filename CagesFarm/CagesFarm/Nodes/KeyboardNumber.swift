8221dbe25792a88560832ae63b8c2aa08dc46d34
Tales Conrado <talesmateuscc@gmail.com>
2021-03-31 11:59:20 -0300

    Adiciona funcionamento do puzzle do teclado

--- CagesFarm/CagesFarm/Nodes/KeyboardNumber.swift
+++ CagesFarm/CagesFarm/Nodes/KeyboardNumber.swift
@@ -10,11 +10,16 @@ import SpriteKit
 class KeyboardNumber: SKSpriteNode {
 
     var digit: SKLabelNode!
+    var numberValue: Int?
     var backgroundOverlay: SKSpriteNode!
+    var shouldHighlight = true
+    var didTouchNumberKey: ((Int) -> Void)?
+    var checkConfirmCancelAction: ((Bool) -> Void)?
 
     convenience init(digit: Int) {
         self.init()
         self.size = CGSize(width: 43, height: 45)
+        self.numberValue = digit
         self.digit = SKLabelNode(text: "\(digit)")
         setupNodes()
     }
@@ -29,7 +34,6 @@ class KeyboardNumber: SKSpriteNode {
         backgroundOverlay.anchorPoint = CGPoint(x: 0.5, y: 0.5)
         backgroundOverlay.alpha = 0.0
         backgroundOverlay.position = CGPoint(x: 1, y: 10)
-        backgroundOverlay.zPosition = +20
         backgroundOverlay.isUserInteractionEnabled = true
         isUserInteractionEnabled = true
         self.addChild(backgroundOverlay)
@@ -40,13 +44,26 @@ class KeyboardNumber: SKSpriteNode {
         digit.fontColor = .white
         digit.fontSize = 17
         digit.fontName = "Dogica"
+        digit.zPosition = +10
         self.addChild(digit)
     }
 
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        if shouldHighlight {
+            let alphaAction1 = SKAction.fadeAlpha(to: 1, duration: 0.05)
+            backgroundOverlay.run(alphaAction1)
+            didTouchNumberKey?(numberValue ?? 0)
+            let isCancel = numberValue == 10 ? true : false
+
+            checkConfirmCancelAction?(isCancel)
+        }
+    }
+
     override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
-        let alphaAction1 = SKAction.fadeAlpha(to: 1, duration: 0.2)
-        let alphaAction2 = SKAction.fadeAlpha(to: 0, duration: 0.2)
-        let actions = SKAction.sequence([alphaAction1, alphaAction2])
-        backgroundOverlay.run(actions)
+        if shouldHighlight {
+            let alphaAction2 = SKAction.fadeAlpha(to: 0, duration: 0.2)
+            let actions = SKAction.sequence([alphaAction2])
+            backgroundOverlay.run(actions)
+        }
     }
 }
